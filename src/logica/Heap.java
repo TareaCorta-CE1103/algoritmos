d4796005ddebc5e873cd960f5f95d98e2223e4ca
ellioth <elliot22rx7@gmail.com>
2015-05-10 12:21:39 -0600

    empezando con el Heap

---
+++ src/logica/Heap.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package logica;
+
+/**
+ *
+ * @author osboxes
+ * @param <dp>
+ */
+public class Heap <dp extends Comparable <dp>>{
+    
+    private dp[] arreglo;
+}
